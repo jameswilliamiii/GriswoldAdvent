34fff3fcd8397cd6acccc401f908b5f36ac53772
James Stubblefield <jameswilliamiii@gmail.com>
2012-11-29 03:11:55 -0600

    updated create action for verifications

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -1,16 +1,10 @@
 class VerificationsController < ApplicationController
   protect_from_forgery :except => ["create"]
-  before_filter :get_user
   
   def create
+    @phone_number = TextMessage.find_by_phone_number(params['From'])
     @phone_number.update_attribute(:verified, true)
     head :ok
   end
-  
-  private
-  def get_user
-    unless @phone_number = TextMessage.find_by_phone_number(params['From'])
-      head :not_found
-    end
-  end
+
 end
