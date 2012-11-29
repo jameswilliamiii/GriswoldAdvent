6b1c474640e6c81382481f97e3a6cb7e292d0752
James Stubblefield <jameswilliamiii@gmail.com>
2012-11-29 04:27:05 -0600

    added get_user method again

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -1,7 +1,14 @@
 class VerificationsController < ApplicationController
-  protect_from_forgery :except => ["create"]
+  # protect_from_forgery :except => ["create"]
+  before_filter :get_user
   
   def create
+    @phone_number.update_attributes(:verified => true)
+    head :ok
+  end
+  
+  private
+  def get_user
     number_from_twilio = params['From']
     cleaned_number = number_from_twilio.gsub(/\D/, "")
     if cleaned_number.start_with? '1'
@@ -9,9 +16,9 @@ class VerificationsController < ApplicationController
       return cleaned_number
     end
     logger.info "*** #{cleaned_number} ***"
-    @phone_number = TextMessage.find_by_phone_number(cleaned_number)
-    @phone_number.update_attributes(:verified => true)
-    head :ok
+    unless @phone_number = TextMessage.find_by_phone_number(cleaned_number)
+      head :not_found
+    end
   end
 
 end
