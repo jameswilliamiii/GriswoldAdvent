5e63078ba3404fc487560d241f8789c00e6f1fdc
James Stubblefield <jameswilliamiii@gmail.com>
2012-11-29 03:50:44 -0600

    added code to clean number from twillo to remove 1

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -4,6 +4,9 @@ class VerificationsController < ApplicationController
   def create
     number_from_twilio = params['From']
     cleaned_number = number_from_twilio.gsub(/\D/, "")
+    if cleaned_number.start_with? '1'
+      cleaned_number[0] = ''
+    end
     logger.debug "*** #{cleaned_number} ***"
     @phone_number = TextMessage.find_by_phone_number(cleaned_number)
     @phone_number.update_attributes(:verified => true)
