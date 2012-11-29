a0bc9cf798ea0630cf0da50bd6d67422f02ef9a5
James Stubblefield <jameswilliamiii@gmail.com>
2012-11-29 03:18:02 -0600

    updated verification create to clean the number from twilio

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -2,7 +2,9 @@ class VerificationsController < ApplicationController
   protect_from_forgery :except => ["create"]
   
   def create
-    @phone_number = TextMessage.find_by_phone_number(params['From'])
+    number_from_twilio = params['From']
+    cleaned_number = number_from_twilio.gsub(/\D/, "")
+    @phone_number = TextMessage.find_by_phone_number(cleaned_number)
     @phone_number.update_attribute(:verified, true)
     head :ok
   end
