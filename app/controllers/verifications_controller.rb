fe1fa90a64c183f3c25c28477921c3b3ef315722
James Stubblefield <jameswilliamiii@gmail.com>
2012-11-29 03:22:39 -0600

    changed verification create to update_attributes

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -5,8 +5,7 @@ class VerificationsController < ApplicationController
     number_from_twilio = params['From']
     cleaned_number = number_from_twilio.gsub(/\D/, "")
     @phone_number = TextMessage.find_by_phone_number(cleaned_number)
-    @phone_number.update_attribute(:verified, true)
-    head :ok
+    @phone_number.update_attributes(:verified => true)
   end
 
 end
