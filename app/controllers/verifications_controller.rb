d3dd35009d64d810d6dbaae9fffc82183060c792
James Stubblefield <jameswilliamiii@gmail.com>
2012-11-29 04:07:10 -0600

    corrected error for cleaning number in validation

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -4,9 +4,10 @@ class VerificationsController < ApplicationController
   def create
     number_from_twilio = params['From']
     cleaned_number = number_from_twilio.gsub(/\D/, "")
-    # if cleaned_number.start_with? '1'
-    #   cleaned_number[0] = ''
-    # end
+    if cleaned_number.start_with? '1'
+      cleaned_number[0] = ''
+      return cleaned_number
+    end
     logger.info "*** #{cleaned_number} ***"
     @phone_number = TextMessage.find_by_phone_number(cleaned_number)
     @phone_number.update_attributes(:verified => true)
