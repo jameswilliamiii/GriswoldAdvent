540761676cd4459b27f232264f70ec5c928da9cf
James Stubblefield <jameswilliamiii@gmail.com>
2012-11-29 04:01:40 -0600

    debug

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -7,7 +7,7 @@ class VerificationsController < ApplicationController
     # if cleaned_number.start_with? '1'
     #   cleaned_number[0] = ''
     # end
-    logger.debug "*** #{cleaned_number} ***"
+    logger.info "*** #{cleaned_number} ***"
     @phone_number = TextMessage.find_by_phone_number(cleaned_number)
     @phone_number.update_attributes(:verified => true)
   end
