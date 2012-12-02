14676ca58e3de316e6befc5cd82d6f2f4de3469e
James Stubblefield <jameswilliamiii@gmail.com>
2012-12-01 22:00:27 -0600

    added twitter gem and created initializer

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = GRISWOLD_TWITTER_CONSUMER_KEY
+  config.consumer_secret = GRISWOLD_TWITTER_CONSUMER_SECRET
+  config.oauth_token = GRISWOLD_TWITTER_OAUTH_TOKEN
+  config.oauth_token_secret = GRISWOLD_TWITTER_OAUTH_TOKEN_SECRET
+end
