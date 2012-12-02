7c46642fd3ac3a2e367fb223de9184f749d2dc2b
James Stubblefield <jameswilliamiii@gmail.com>
2012-12-01 22:06:42 -0600

    fixxed error on ENV vars for Twitter

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key = GRISWOLD_TWITTER_CONSUMER_KEY
-  config.consumer_secret = GRISWOLD_TWITTER_CONSUMER_SECRET
-  config.oauth_token = GRISWOLD_TWITTER_OAUTH_TOKEN
-  config.oauth_token_secret = GRISWOLD_TWITTER_OAUTH_TOKEN_SECRET
+  config.consumer_key = ENV['GRISWOLD_TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['GRISWOLD_TWITTER_CONSUMER_SECRET']
+  config.oauth_token = ENV['GRISWOLD_TWITTER_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['GRISWOLD_TWITTER_OAUTH_TOKEN_SECRET']
 end
