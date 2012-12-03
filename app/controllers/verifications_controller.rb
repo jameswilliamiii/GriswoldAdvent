7d90326cce57e5f7f6c7e420e35cac14abbc5f25
James Stubblefield <jameswilliamiii@gmail.com>
2012-12-03 17:37:57 -0600

    removed head in the verifications controller

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -7,19 +7,16 @@ class VerificationsController < ApplicationController
     if message_from_twilio.downcase == 'stop'
       phone_number = TextMessage.find_by_phone_number(cleaned_number)
       phone_number.destroy
-      head :ok
     else
       phone_number = TextMessage.find_by_phone_number(cleaned_number)
       if phone_number.present?
         phone_number.verified = true
         phone_number.save
-        head :ok
       else
         text_message = TextMessage.new
         text_message.phone_number = cleaned_number
         text_message.verified = true
         text_message.save
-        head :ok
       end
     end
     render 'verified.xml.erb', :content_type => 'text/xml'
