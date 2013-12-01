cf19a4870b0ef8e43cbc4a91d2e8fb649040f002
James Stubblefield <jameswilliamiii@gmail.com>
2013-12-01 15:56:18 -0600

    modiy admin panel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,7 @@
 class AdminController < ApplicationController
   before_filter :authorize
-  
+
   def index
-    @text_messages_count = TextMessage.count
-    @text_messages_verified = TextMessage.order(:created_at).where(:verified => true)
+    @text_messages = TextMessage.order(:created_at)
   end
 end
